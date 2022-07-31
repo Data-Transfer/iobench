86619a14acf084cbf0b14a0a8a01ff62a01b502a
Ugo Varetto <ugovaretto@gmail.com>
2022-07-31 22:36:01 +0800

    Checking for short reads/writes in p* functions

--- src/vec_io/mod.rs
+++ src/vec_io/mod.rs
@@ -24,17 +24,29 @@ pub fn read_vec_slice(
             iov_base: buf[b..e].as_mut_ptr() as *mut c_void,
             iov_len: (e - b) as size_t,
         };
+        println!("iovec size: {}", iovec.iov_len);
         iovecs.push(iovec);
         r += e - b;
     }
     unsafe {
-        if readv(fd, iovecs.as_ptr() as *const IoVec, iovecs.len() as c_int) < 0  {
+        // @WARNING: partial reads possible, normally ok when total size < 2GiB
+        let b = readv(fd, iovecs.as_ptr() as *const IoVec, iovecs.len() as c_int);
+        if b != r as ssize_t  {
             Err(std::io::Error::last_os_error())
         } else {
             Ok(())
         }
     }
 }
+// for (;;) {
+//     written = writev(fd, iov+cur, count-cur);
+//     if (written < 0) goto error;
+//     while (cur < count && written >= iov[cur].iov_len)
+//         written -= iov[cur++].iov_len;
+//     if (cur == count) break;
+//     iov[cur].iov_base = (char *)iov[cur].iov_base + written;
+//     iov[cur].iov_len -= written;
+// }
 
 // ----------------------------------------------------------------------------
 pub fn read_vec_slice_offset(
@@ -81,7 +93,7 @@ pub fn write_vec_slice(file: &std::fs::File, buf: &[u8], chunk_size: u64) -> std
         r += chunk_size as usize;
     }
     unsafe {
-        if writev(fd, iovecs.as_ptr() as *const IoVec, iovecs.len() as c_int) < 0 {
+        if writev(fd, iovecs.as_ptr() as *const IoVec, iovecs.len() as c_int) != r as ssize_t {
             Err(std::io::Error::last_os_error())
         } else {
             Ok(())
@@ -105,7 +117,7 @@ pub fn write_vec_slice_offset(file: &std::fs::File, buf: &[u8], chunk_size: u64,
         r += chunk_size as usize;
     }
     unsafe {
-        if pwritev(fd, iovecs.as_ptr() as *const IoVec, iovecs.len() as c_int, offset as off_t) < 0 {
+        if pwritev(fd, iovecs.as_ptr() as *const IoVec, iovecs.len() as c_int, offset as off_t) != r as ssize_t {
             Err(std::io::Error::last_os_error())
         } else {
             Ok(())
